35d23442b9cba2ff2a6da4480c2ff5024ee4a1ef
WretchedEraser <brendanhawthorne@gmail.com>
2016-05-27 21:46:00 -0700

    added more scenes
    added

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
-//
-// $('.sidebar').first()
-//   .sidebar('attach events', '.toggle.button')
-// ;
-// $('.menu').hide();
-var a=document.getElementsByTagName("a");
-for(var i=0;i<a.length;i++)
-{
-    a[i].onclick=function()
-    {
-        window.location=this.getAttribute("href");
+//prevent anchor link default on mobile web app
+var a = document.getElementsByTagName("a");
+for (var i = 0; i < a.length; i++) {
+    a[i].onclick = function() {
+        window.location = this.getAttribute("href");
         return false;
     };
 }
+$('#showModalBtn').click(function() {
+    $('.ui.long.modal').modal('show');
+});
