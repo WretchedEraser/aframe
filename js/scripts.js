87e3318de4c8b96ed77669a7b3cb1865a1707fb6
WretchedEraser <brendanhawthorne@gmail.com>
2016-05-27 23:19:55 -0700

    adjusted z-index

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-addToHomescreen({
-    mandatory: true
-});
+addToHomescreen({});
 //prevent anchor link default on mobile web app
 var a = document.getElementsByTagName("a");
 for (var i = 0; i < a.length; i++) {
